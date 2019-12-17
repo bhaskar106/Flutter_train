a70f13fefb261cede99c8fd420b8213404e4a724
bhaskar106 <54832857+bhaskar106@users.noreply.github.com>
2019-12-17 18:31:06 +0530

    Add files via upload

---
+++ int_double.dart
@@ -0,0 +1,22 @@
+void main() {
+  add(a,b){
+    int c=a+b;
+    print("Addition result is $c");
+  }
+  subtract(a,b){
+    int c=a-b;
+    print("Subtraction result is $c");
+  }
+  multiple(a,b){
+    double c=a*b;
+    print("Multiplication result is $c");
+  }
+  divide(a,b){
+    double c=a/b;
+    print("Division result is $c");
+  }
+  add(5,56);
+  subtract(188,34);
+  multiple(26,6.54);
+  divide(6473,345);
+}
