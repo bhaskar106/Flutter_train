fd11d4f2b243f182e0b8b7f39f9e6ab841d39a7f
bhaskar106 <54832857+bhaskar106@users.noreply.github.com>
2019-12-17 19:09:28 +0530

    Add files via upload

---
+++ String.dart
@@ -0,0 +1,16 @@
+void main() {
+concat(a,b){
+  String c=a+b;
+  print("Concatination of the strings are '$c'");
+}
+length(a){
+  int b=a.length;
+  print("Lenght of the given string is '$b'");
+}
+split(a){
+  print("String after spliting is ${a.split(' ')}");
+}
+concat('implimen','tation');
+length('Birthday');
+split('Sanjay varma');
+}
