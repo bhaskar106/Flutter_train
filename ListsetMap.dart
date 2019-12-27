7d4f07bf6043ee7e2abafd23a5721999ff923ac6
bhaskar106 <54832857+bhaskar106@users.noreply.github.com>
2019-12-27 11:27:43 +0530

    Update ListsetMap.dart

--- ListsetMap.dart
+++ ListsetMap.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 void main() {
 
-  var  list1 = new List();
+  List list1 = new List();
        list1.add(34);       
        list1.add(876);
        list1.add(2);
@@ -11,20 +11,20 @@ void main() {
        print(list1);
        print("list without limit");
        print("Enter the value to add into the list:");
-  var  AV = int.parse(stdin.readLineSync());
+  int  AV = int.parse(stdin.readLineSync());
        list1.add(AV);
        print(list1);
        print("Enter the index and value where you want to update:");
-  var  UI = int.parse(stdin.readLineSync());
-  var  UV = int.parse(stdin.readLineSync());
+  int  UI = int.parse(stdin.readLineSync());
+  int  UV = int.parse(stdin.readLineSync());
        list1[UI]= UV;
        print(list1);
        print("Enter the index number of the value to be removed from the list:");
-  var  RI = int.parse(stdin.readLineSync());
+  int  RI = int.parse(stdin.readLineSync());
        list1.removeAt(RI);
        print(list1);
        print("Enter the Element to be removed:");
-  var  RV = int.parse(stdin.readLineSync());
+  int  RV = int.parse(stdin.readLineSync());
        for(int i=list1.length-1;i>=0;i--)
        {
         if(RV == list1[i])
@@ -37,20 +37,20 @@ void main() {
        print(list1);
        
 
-  var list2 = new List(2);
-      list2[0]=4;
-      list2[1]=35;
-      print("list with limit");
-      print(list2);
+  List list2 = new List(2);
+       list2[0]=4;
+       list2[1]=35;
+       print("list with limit");
+       print(list2);
 
   
-  var list3 = new List();
-      list3 = ['how','are','you'];
-      print("list without limit and zero size");
-      print(list3);
-      print(list3[1]);
+  List list3 = new List();
+       list3 = ['how','are','you'];
+       print("list without limit and zero size");
+       print(list3);
+       print(list3[1]);
   
-  var students =  new Set();
+  Set students =  new Set();
       students.add("student1");
       students.addAll(['std2','std3','std4']);
       print(students);
@@ -63,12 +63,12 @@ void main() {
       students.remove(RSV);
       print(students);
       print("Enter the index where the value should be removed:");
-  var RSI = int.parse(stdin.readLineSync());
+  int RSI = int.parse(stdin.readLineSync());
       students.remove(students.elementAt(RSI));
       print(students);
       
   
-  var student = new Map();
+  Map student = new Map();
       student['std1']='Sanjay';
       student['std2']='bhaskar';
       student['std3']='sanjay';
@@ -112,7 +112,7 @@ void main() {
       print(student);
       
      
-  var employee = {'empl1':'sanjay','empl2':'bhaskar','empl3':''};
+  Map employee = {'empl1':'sanjay','empl2':'bhaskar','empl3':''};
       print("Map of type literal");
       print(employee);
       print(employee['empl1']);
