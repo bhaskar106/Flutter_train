0b3b47dc1242efd7b8510a0a5e0d6d42d5de3833
bhaskar106 <54832857+bhaskar106@users.noreply.github.com>
2020-01-24 10:21:43 +0530

    Add files via upload

--- Json.dart
+++ Json.dart
@@ -1,13 +1,14 @@
 import 'dart:convert' as JSON;
+import 'dart:io';
 main() {
+  int dt1 = DateTime.now().millisecond;
   String intInJson = '{"integer":123}';
   intInJson = '{"integer123":"86"}';
   String doubleInJson = '{"Double":479.007}';
   String boolInJson = '{"Bool":"true"}';
   String stringInJson = '{"String":"Testing"}';
   Map objectInJson = {"a": 1, "b": "c"};
-  String arrayInJson =
-      '{"Array":[{"a":1,"b":"text"},{"a":2,"b":"text2"},{"a":3,"b":"text3"}]}';
+  String arrayInJson ='{"Array":[{"a":1,"b":"text"},{"a":2,"b":"text2"},{"a":3,"b":"text3"}]}';
   String classInJson = '{"Student":[{"name":"testing","rollNumber":"04","attendancePercentage":99.75,"subjectAndMarks":{"maths":95,"english":84,"social":78},"certificatesEarned":["10th","inter","b-tech","m-tech"]}]}';
   String nestedJson = '{"Student": [{"1stclass": [{"Name": "student1","RollNumber":25,"Exams": {"1stexam": {"Marks": {"Maths": 100,"English": 78,"science": 79,"Drawing": 97}}}}],"2ndclass": [{"Name": "student2","RollNumber": 7,"Exams": {"1stexam": {"Marks": {"Maths": 89,"English": 90,"science": 69,"Drawing": 40}}}}],"3rdclass": [{"Name": "student3","RollNumber": 106,"Exams": {"1stexam":{"Marks": {"Maths": 99,"English": 94,"science": 95,"Drawing": 96}}}}]}]}';
   print(intInJson);
@@ -29,6 +30,138 @@ main() {
   print(json3["Student"][0]["1stclass"][0]["Exams"]["1stexam"]["Marks"]["Maths"]);
   json3["Student"][0]["1stclass"][0]["Exams"]["1stexam"]["Marks"]["Maths"]=99;
   print(json3["Student"][0]["1stclass"][0]["Exams"]["1stexam"]["Marks"]);
-  print(json3);
+  print('\n$json3');
+  //json3["Student"][0]["1stclass"][0]["Exams"]["1stexam"]["Marks"].remove('Maths');
+  //print("\n$json3");
+  String keyvalue =  stdin.readLineSync();
+  dynamic find(String key,Map json)
+  {
+    if(json[key] is Map)
+    {
+       Map newmap = json[key];
+       if(newmap[keyvalue]!= null)
+       {
+         print(newmap[keyvalue]);
+       }
+       else
+       {
+        for(String key in newmap.keys)
+        {
+          var val = find(key,newmap);
+            if(val!=null)
+            {
+              print(val);
+            }
+        }
+       }
+    }
+    else if(json[key] is List)
+    {
+       List newlist = json[key];
+       for(int i=0;i<=newlist.length-1;i++)
+       { 
+        if(newlist[i] is Map)
+        {
+          Map newmap = newlist[i];
+          if(newmap[keyvalue]!=null)
+          {
+            print(newmap[keyvalue]);
+          }
+          else
+          {
+            for(String key in newmap.keys)
+            {
+              var val = find(key,newmap);
+              if(val!=null)
+              {
+                print(val);
+              }
+            }
+          }
+        }
+        // else if(newlist[i] is List)
+        // {
+        //   List newlist = newlist[i];
+        //   for(int i=0;i<=newlist.length-1;i++)
+        //   {
+        //     if(newlist[i] is Map)
+        //     {
+        //       Map newmap = newlist[i];
+        //       if(newmap[keyvalue]!=null)
+        //       {
+        //       print(newmap[keyvalue]);
+        //       }
+        //       else
+        //       {
+        //         for(String key in newmap.keys)
+        //         {
+        //           var val = find(key,newmap);
+        //           if(val!=null)
+        //           {
+        //             print(val);
+        //           }
+        //         }
+        //       }
+        //     }
+        //   }
+        // }
+       }
+    }
+  }
+  dynamic remove(String key,Map json)
+  {
+    if(json[key] is Map)
+    {
+      Map newmap = json[key];
+      if(newmap[keyvalue]!= null)
+      {
+        newmap.remove(keyvalue);
+      }
+      else
+      {
+        for(String key in newmap.keys)
+        {
+          var val = remove(key,newmap);
+            if(val!=null)
+            {
+              newmap.remove(val);
+            }
+        }
+      }
+    }
+    else if(json[key] is List)
+    {
+      List newlist = json[key];
+      for(int i=0;i<=newlist.length-1;i++)
+      { 
+        if(newlist[i] is Map)
+        {
+         Map newmap = newlist[i];
+         if(newmap[keyvalue]!=null)
+         {
+          newmap.remove(keyvalue);
+         }
+         else
+         {
+          for(String key in newmap.keys)
+          {
+           var val = remove(key,newmap);
+            if(val!=null)
+            {
+              newmap.remove(val);
+            }
+          }
+        }
+      }
+     }
+    }
+  }
+  find("Student",json3);
+  remove("Student",json3);
+  print(json3[json3.keys.elementAt(0)][0]);
+  //if(key = )
+  int dt2 = DateTime.now().millisecond;
+  print("${dt2-dt1} milli sec");
+  
 }
 
